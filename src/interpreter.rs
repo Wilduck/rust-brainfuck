4789bd7df60845ee40ebe1f81be5da73d959f0cd
Erik Swanson <theerikswanson@gmail.com>
2015-05-25 23:17:23 -0400

    Use a state struct rather than individual local variables.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,75 +3,88 @@ use std::char;
 use super::tokenize;
 
 
+struct State<MemType> {
+    memory: MemType,
+    code_loc: usize,
+    data_loc: usize,
+    input_loc: usize,
+    loop_depth: usize,
+    loop_loc: Vec<usize>,
+}
+
+
 pub fn interpret(tokens: Vec<tokenize::Operator>, input: String) -> [u8; 30000] {
+    let mut state = State {
+        memory: [0; 30000],
+        code_loc: 0,
+        data_loc: 0,
+        input_loc: 0,
+        loop_depth: 0,
+        loop_loc: Vec::new()
+    };
     let instructions = tokens.len();
-    let mut memory: [u8; 30000] = [0; 30000];
     let input_bytes = input.as_bytes();
-    let mut code_loc: usize = 0;
-    let mut input_loc: usize = 0;
-    let mut loop_loc: Vec<usize> = Vec::new();
-    let mut data_loc: usize = 0;
-    while code_loc < instructions {
-        let ref operator = tokens[code_loc];
+    while state.code_loc < instructions {
+        let ref operator = tokens[state.code_loc];
         match *operator {
             tokenize::Operator::IncCell => {
-                memory[data_loc] += 1;
-                code_loc += 1;
+                state.memory[state.data_loc] += 1;
+                state.code_loc += 1;
             }
             tokenize::Operator::DecCell => {
-                memory[data_loc] -= 1;
-                code_loc += 1;
+                state.memory[state.data_loc] -= 1;
+                state.code_loc += 1;
             }
             tokenize::Operator::IncPtr => {
-                data_loc += 1;
-                code_loc += 1;
+                state.data_loc += 1;
+                state.code_loc += 1;
             }
             tokenize::Operator::DecPtr => {
-                data_loc -= 1;
-                code_loc += 1;
+                state.data_loc -= 1;
+                state.code_loc += 1;
             }
             tokenize::Operator::Print => {
-                let character = char::from_u32(memory[data_loc] as u32);
+                let character = char::from_u32(state.memory[state.data_loc] as u32);
                 match character {
                     Some(c) => print!("{}", c),
                     None => print!("<??>"),
                 }
-                code_loc += 1;
+                state.code_loc += 1;
             }
             tokenize::Operator::Read => {
-                memory[data_loc] = input_bytes[input_loc];
-                input_loc += 1;
-                code_loc += 1;
+                state.memory[state.data_loc] = input_bytes[state.input_loc];
+                state.input_loc += 1;
+                state.code_loc += 1;
             }
             tokenize::Operator::JumpZero => {
-                let mut depth_tracker = 0;
-                if memory[data_loc] == 0 {
-                    while (tokens[code_loc] != tokenize::Operator::Loop) 
-                        | (depth_tracker != 0) {
-                        if tokens[code_loc] == tokenize::Operator::JumpZero {
-                            depth_tracker += 1;
-                        } else if tokens[code_loc] == tokenize::Operator::Loop {
-                            depth_tracker -= 1;
+                state.loop_depth = 0;
+                if state.memory[state.data_loc] == 0 {
+                    while (tokens[state.code_loc] != tokenize::Operator::Loop)
+                        | (state.loop_depth != 0) {
+                        if tokens[state.code_loc] == tokenize::Operator::JumpZero {
+                            state.loop_depth += 1;
+                        } else if tokens[state.code_loc] == tokenize::Operator::Loop {
+                            state.loop_depth -= 1;
                         }
-                        code_loc += 1;
+                        state.code_loc += 1;
                     }
                 } else {
-                    loop_loc.push(code_loc);
-                    code_loc += 1;
+                    state.loop_loc.push(state.code_loc);
+                    state.code_loc += 1;
                 }
             }
             tokenize::Operator::Loop => {
-                if memory[data_loc] == 0 {
-                    loop_loc.pop();
-                    code_loc += 1;
+                if state.memory[state.data_loc] == 0 {
+                    state.loop_loc.pop();
+                    state.code_loc += 1;
                 } else {
-                    code_loc = match loop_loc.pop() {
+                    state.code_loc = match state.loop_loc.pop() {
                         Some(loc) => loc,
-                        None => code_loc + 1,
+                        None => state.code_loc + 1,
                     }
                 }
             }
         }
     }
-    memory
+    state.memory
 }
